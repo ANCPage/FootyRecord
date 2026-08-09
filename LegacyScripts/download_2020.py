431a1cb10382b3601ceb793cfeaf4126d0cdf753
Austin <austin@localhost>
2026-08-10 00:57:38 +1000

    Archive LegacyScripts + engine_data.py.bak (audit #23)
    Removed from the tree; preserved forever in git history (baseline commit).

--- LegacyScripts/download_2020.py
+++
@@ -1,9 +0,0 @@
-﻿import sys
-import os
-
-sys.path.append(os.path.join(os.getcwd(), 'Core'))
-from engine_scraper import update_all_data
-import config
-
-if __name__ == '__main__':
-    update_all_data(config.DATA_DIR, year=2020, force_rebuild=False)
